fd2872649fc22afb3ad3f95bab35d0096db29e69
4cc3ss <paul.searon@sky.com>
2024-10-23 21:59:50 +0100

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite2 = sprites.create(assets.image`bullet`, SpriteKind.Projectile)
-    mySprite2.setPosition(79, 105)
+    mySprite2 = sprites.createProjectileFromSprite(assets.image`bullet`, mySprite, 0, 0)
     mySprite2.setVelocity(0, -100)
+    mySprite2 = sprites.create(assets.image`no bullet`, SpriteKind.Projectile)
     music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.player1.moveSprite(mySprite)
+    controller.moveSprite(mySprite)
     controller.moveSprite(mySprite2)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.player1.moveSprite(mySprite)
+    controller.moveSprite(mySprite)
     controller.moveSprite(mySprite2)
 })
 let mySprite2: Sprite = null
